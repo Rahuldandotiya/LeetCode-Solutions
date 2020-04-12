1c6ecd1713f6747dbe30ca47403f111aefb51ee9
Rahul dandotita <36420911+Rahuldandotiya@users.noreply.github.com>
2020-04-12 19:02:12 +0530

    Create ContainsDuplicate.cpp
    Easy

---
+++ ContainsDuplicate.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    bool containsDuplicate(vector<int>& nums) {
+        if(nums.size()==0 or nums.size()==1) return false;
+        unordered_set<int> s;
+        for(int i=0;i<nums.size();i++){
+            if(s.find(nums[i])==s.end()){
+                s.insert(nums[i]);
+            }
+            else return true;
+        }
+        return false;
+    }
+};
